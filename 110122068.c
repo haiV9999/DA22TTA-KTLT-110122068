a2b47c9f93de7599d63d68ab86c3d8662c224c88
Võ Chí Hải <vochihai.010104@gmail.com>
2026-01-29 07:36:23 +0700

    Add C program of <110122068_VoChiHai>

---
+++ 110122068.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Nhap bien n: ");
+    scanf("%d", &n);
+
+    int tong = n;
+    printf("Tong: %d", tong);
+
+    int hieu = n;
+    printf("\nHieu: %d", hieu);
+
+    int tich = n;
+    printf("\nTich: %d", tich);
+
+    float thuong = (float)n / 2;
+    printf("\nThuong: %.2f", thuong);
+
+    return 0;
+}
